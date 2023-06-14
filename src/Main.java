80600a573e1fb00ae4b16207828ce241217c68cb
Ariel <135558227+OrielTheCOOKIE@users.noreply.github.com>
2023-06-14 16:55:27 +0000

    changing values

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,17 @@ class Main {
     public static void main(String[] args) {
         int numberOfMotors =4;
         System.out.println(numberOfMotors);
-
+        
+        numberOfMotors =6;
+        System.out.println(numberOfMotors);
     }
 }
+        
+
+
+
+
+
      // functions are also known as methods and have to be in a class, to put simply functions are actions
      /*Conventions:PascalNamingConvention-classes, camelCaseNaming Convention-function
       * Void= does not give back or you dont recieve data/info
