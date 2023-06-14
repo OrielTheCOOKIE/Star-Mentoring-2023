1f41d4ac092029993f1586682ab908336e36c0bc
Ariel <135558227+OrielTheCOOKIE@users.noreply.github.com>
2023-06-14 00:18:35 +0000

    notes

--- src/Main.java
+++ src/Main.java
@@ -5,3 +5,8 @@ class Main {
 
     }
 }
+     // functions are also known as methods and have to be in a class, to put simply functions are actions
+     /*Conventions:PascalNamingConvention-classes, camelCaseNaming Convention-function
+      * Void= does not give back or you dont recieve data/info
+      * To print text or show text- System.out.println("");
+      */
