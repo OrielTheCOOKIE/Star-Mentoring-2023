39d06d8eef3c2246bd9a4955a4a7cb1d57cc6677
Ariel <135558227+OrielTheCOOKIE@users.noreply.github.com>
2023-06-19 03:23:06 +0000

    left&right motors

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import ftc.Motor;
+import ftc.Sleep;
+
+public class Main {
+    public static void main(String[] args) {
+        Motor lMotor = new Motor(1);
+        Motor rMotor = new Motor(2);
+
+        lMotor.setPower(0.5);
+        rMotor.setPower(0.5);
+
+        Sleep.forMilliseconds(3000);
+}
+
+}
